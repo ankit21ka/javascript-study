86f73859dca900bc79aa2f1adc66c95bf737e853
Ankit kumar <ankit21ka@gmail.com>
2025-09-23 15:42:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+console.log("variables");
+
+const accountId = 3116
+let accountEmail ="ankit21@gmail.com"
+var accountPassword = "212003"
+accountCity = "Phagwara"
+
+
+// accountId = 3118
+
+accountEmail = "a8528874374@gmail.com"
+accountPassword = '212121'
+accountCity = "jalandhar"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
