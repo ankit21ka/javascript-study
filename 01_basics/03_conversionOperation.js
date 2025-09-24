623d9b5336e70a7549e5a9d6c72f347d994c6649
Ankit kumar <ankit21ka@gmail.com>
2025-09-24 15:10:26 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+
+// let score = null
+// let score = "33abc"
+// let score = undefined
+// let score = true
+// let score = "ankit"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1    //true
+// let isLoggedIn = ""      //false
+// let isLoggedIn = "ankit"      //true
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false ;  
+// "ankit" => true
+
+
+let someNumber = 33
+
+let stringIssomeNumber = String(someNumber);
+console.log(stringIssomeNumber);
+console.log(typeof stringIssomeNumber);
