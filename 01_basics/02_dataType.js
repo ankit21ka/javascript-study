caff1456ed9c9f55d591e7dd755c1f00ed004bd8
Ankit kumar <ankit21ka@gmail.com>
2025-09-24 14:34:51 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,38 @@
+"use strict";  //treat all JS code as never version
+
+// alert("hello ankit");  //we are using nodejs, not browser
+
+// console.log(3
+//     +
+//      3);   //code readability should be high
+
+console.log("Ankit");
+
+
+let name = "ankit";
+let age = 22;
+let isLoggedIn = false
+let state = null;
+let rollno;
+
+// premitive datatype 
+
+
+// number => 2 to power of 53
+// bigint  => for bigger numbers then number datatype
+// string => "" or ''
+// boolean => true or false
+// null =>  standalone value
+// undefined => when we declare variable or nor declare the value
+// symbol => unique 
+
+
+
+// object
+
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
