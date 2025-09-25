8363a31dc247e6c4f75b9fdf1f3a6156086e55b8
Ankit kumar <ankit21ka@gmail.com>
2025-09-25 15:01:07 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,8 +32,54 @@
 // "ankit" => true
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringIssomeNumber = String(someNumber);
-console.log(stringIssomeNumber);
-console.log(typeof stringIssomeNumber);
+// let stringIssomeNumber = String(someNumber);
+// console.log(stringIssomeNumber);
+// console.log(typeof stringIssomeNumber);
+
+
+// **************operation**************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " ankit"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(3 + 4 * 4 % 3);
+
+// console.log(true);  //true
+// console.log(+true);  //1
+// console.log(true+);  //error
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter++ //101
+// ++gameCounter
+// gameCounter-- //99
+// --gameCounter
+console.log(gameCounter);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,36 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2<=1);
+// console.log(2 == 1);
+// console.log(1 != 1);
+
+
+
+// ----------------can't use this type of conversion --------
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
+// console.log(null > 0);  //false
+// console.log(null == 0);  //false
+// console.log(null >= 0);  //true
+
+/*the reason is that an qualtiy check == and comparison 
+<, >, >=, <=, work diferently.
+Comparison convert null to a number , treating it as 0.
+that's why (3) null >=0 is true and (1) null > 0 is false.
+*/
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+
+// -----------------------------------------------------------
+
+// === 
+
+console.log("2" == 2); //true
+console.log("2" === 2); //false
