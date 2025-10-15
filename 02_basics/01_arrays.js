a8a68a0dd8c6fe6aedd67226486b41795dd2df07
Ankit kumar <ankit21ka@gmail.com>
2025-10-15 17:44:04 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,53 @@
+// array
+
+const myArr = [0, 1, 2, 3,  4, 5, 6]
+// console.log(myArr[7]);
+
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myHeros[1]);
+
+
+// Array methods
+
+// myArr.push(7)   //its help to add array on at the end
+// myArr.push(0)   
+// myArr.pop()   //its help to remove array on at the end
+
+// myArr.unshift(9)  //its help to add array on at the first
+// myArr.shift()  //its help to remove array on at the first
+
+// console.log(myArr.includes(9));  //false  //it help to find numbers on array if this is not avaliable the its give you false 
+// console.log(myArr.indexOf(6));  //false  //its help to find the index of any number or any array
+
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof myArr);   //object
+// console.log(newArr);
+// console.log(typeof newArr);  //string
+
+
+
+// slice , splice
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1,3);
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log("C ", myArr);
+
+console.log(myn2);
+
+
+// array part 2 video
+
+
+
+
+
